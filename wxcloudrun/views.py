5cfd44e3ae0ad596813c8416350731f94d2f9ece
hewei92 <Wei.He@jd.com>
2022-08-17 12:42:30 +0800

    x

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -104,7 +104,7 @@ def push(request, _):
         "template_id": "Akw6PSGAcqzp0Ws12sv_Ug1HFaWRhS-6r2hJO0GezQo",
         "page": "pages/index",
         "touser": "o29zt5JTOPF2SB-JX_dGwDfFT1ak",
-        "data": {"上次记录体重": "value1", "上次记录时间": "value2"},
+        "data": {"上次记录体重": { "value": "value1"}, "上次记录时间": { "value": "value2"} },
         "miniprogram_state": "trial",
         "lang": "zh_CN"
     }
